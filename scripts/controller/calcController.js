a8e689c77d6fe58de738b7f68fdc4ac0fd6e568a
alexandre-campos-flooxai <alexandre.campos.flooxai@gmail.com>
2025-03-18 18:02:11 -0300

    update

--- scripts/controller/calcController.js
+++ scripts/controller/calcController.js
@@ -1,5 +1,6 @@
 class CalcController {
   constructor() {
+    this._operation = [];
     this._displayCalcEl = document.querySelector('#display');
     this._dataEl = document.querySelector('#data');
     this._timeEl = document.querySelector('#hora');
@@ -32,16 +33,88 @@ class CalcController {
     });
   }
 
+  clearAll() {
+    this._operation = [];
+  }
+
+  clearEntry() {
+    this._operation.pop();
+  }
+
+  getLastOperation() {
+    return this._operation[this._operation.length - 1];
+  }
+
+  addOperation(value) {
+    if (isNaN(this.getLastOperation())) this._operation.push(value);
+
+    console.log(this._operation);
+  }
+
+  setError() {
+    this.displayCalc = 'Error';
+  }
+
+  execBtn(value) {
+    switch (value) {
+      case 'ac':
+        this.clearAll();
+        break;
+      case 'ce':
+        this.clearEntry();
+        break;
+      case 'soma':
+        break;
+
+      case 'subtracao':
+        break;
+
+      case 'divisao':
+        break;
+
+      case 'multiplicacao':
+        break;
+
+      case 'porcento':
+        break;
+
+      case 'igual':
+        break;
+
+      case 'ponto':
+        break;
+
+      case '0':
+      case '1':
+      case '2':
+      case '3':
+      case '4':
+      case '5':
+      case '6':
+      case '7':
+      case '8':
+      case '9':
+        this.addOperation(parseInt(value));
+        break;
+
+      default:
+        this.setError;
+        break;
+    }
+  }
+
   initButtonsEvents() {
     let buttons = document.querySelectorAll('#buttons > g, #parts > g');
 
     buttons.forEach((btn) => {
       this.addEventListenerAll(btn, 'click drag', () => {
-        console.log(btn.className.baseVal.replace('btn-', ''));
+        let textBtn = btn.className.baseVal.replace('btn-', '');
+
+        this.execBtn(textBtn);
       });
 
       this.addEventListenerAll(btn, 'mouseover mouseup mousedown', (e) => {
-        btn.computedStyleMap.cursor = 'pointer';
+        btn.style.cursor = 'pointer';
       });
     });
   }
